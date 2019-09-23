bc4f5fa85288fa2e33d42533dd37cfd0d1dc1da8
istvan <bucsistvan99@gmail.com>
2019-09-23 20:28:43 +0200

    A MemoRize program alaplogikájának elkezdése

--- MemoRize/src/main/java/game/controller/App.java
+++ MemoRize/src/main/java/game/controller/App.java
@@ -1,6 +1,8 @@
 package game.controller;
 
+import game.model.GameData;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
@@ -9,8 +11,12 @@ import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 public class App extends Application {
-    public void start(Stage stage){
+    GameData gameData = new GameData();
+    public void start(Stage stage) throws InterruptedException {
         /*fxData.setStage(stage);
 
         //főmenü scene
@@ -32,9 +38,121 @@ public class App extends Application {
 
         fxData.changeScene(mainMenuScene);
         stage.show();*/
+        setUp();
+        stage.setScene(gameData.getMainScene());
+
+        //5 random gombot kell megnyomni
+        //randomNumbers(5);
+        //kiir();
+        stage.show();
+        //TimeUnit.SECONDS.sleep(10);
+        //System.out.println("p;lda");
+    }
+
+    //felsetupolja a scene-t, valamint néhány egyébb változót
+    public void setUp(){
+        gameData.setMainGroup(new Group());
+        gameData.setStart(new Button("Start"));
+        gameData.getStart().setLayoutX(40);
+        gameData.getStart().setLayoutY(350);
+        gameData.getStart().setMinSize(25, 25);
+        gameData.getStart().setId(Integer.toString(0));
+        gameData.getMainGroup().getChildren().add(gameData.getStart());
+
+        gameData.setButtons(gombolo());
+
+        gameData.getMainGroup().getChildren().addAll(gameData.getButtons());
+        gameData.setMainScene(new Scene(gameData.getMainGroup(),600,400));
+
+        tableHandler(gameData.getButtons());
+        gameData.setHaveToPress(new Boolean[36]);
+        allFalse();
+
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    //A tábla kezeléséért felel
+    public void tableHandler(Button[] buttons){
+        for (int i=0; i<buttons.length; i++){
+            final int j=i;
+            buttons[i].setOnAction(event->buttonHandler(buttons[j]));
+        }
+        gameData.getStart().setOnAction(event -> {
+            randomNumbers(5);
+        });
+        //kiir();
+    }
+
+    //A gombok kezeléséért felel a táblán
+    public void buttonHandler(Button button){
+        int id=Integer.parseInt(button.getId());
+        if(id==0) {
+            button.setId(Integer.toString(1));
+            button.setStyle("-fx-background-color: #0080ff "); //a gomb színe kék színre vált
+            //button.setStyle("-fx-background-color: #00ff00; "); //a gomb színe z0ld szinre vált
+        }else{
+            button.setId(Integer.toString(0));
+            button.setStyle("-fx-background-color: #808080 "); //a gomb színe szürke szinre vált
+        }
+    }
+
+    //ezt a részt fogja felváltani a view, jelenleg csak sablonként használ egy kézzel setupolt gombot
+    public Button[] gombolo(){
+        Button[] buttons = new Button[36];
+        for (int i = 0; i <buttons.length ; i++) {
+            buttons[i] = new Button();
+            buttons[i].setLayoutX(30*(i%6));
+            buttons[i].setLayoutY(30*(i/6));
+            buttons[i].setMinSize(25, 25);
+            buttons[i].setId(Integer.toString(0));
+            buttons[i].setStyle("-fx-background-color: #808080 ");
+        }
+        return buttons;
+    }
+
+    //beállítja azt, hogy egyik gomb sincs megnyomva
+    public void allFalse(){
+        for (int i = 0; i <36 ; i++) {
+            gameData.getHaveToPress()[i]=false;
+        }
+    }
+    public void allDisable(){
+        for (int i = 0; i <gameData.getButtons().length ; i++) {
+            gameData.getButtons()[i].setStyle("-fx-background-color: #808080 ");
+        }
+    }
+
+    //random kiválasztja, hogy melyik gombokat kell megnyomni
+    public void randomNumbers(int db){
+        allFalse();
+        allDisable();
+        Random random = new Random();
+        int rand;
+        int i=0;
+        while (i<db){
+            i++;
+            rand = random.nextInt(36);
+            while (gameData.getHaveToPress()[rand]){
+                rand = random.nextInt(36);
+            }
+            gameData.getHaveToPress()[rand]=true;
+            gameData.getButtons()[rand].setStyle("-fx-background-color: #00ff00; ");
+            try {
+                TimeUnit.MILLISECONDS.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //kiírja az összes gomb állapotát(meg kell nyomni vagy sem)
+    public void kiir(){
+        for (int i = 0; i <gameData.getHaveToPress().length ; i++) {
+            System.out.println(gameData.getHaveToPress()[i]);
+        }
+    }
+
 }
--- MemoRize/src/main/java/game/model/GameData.java
+++ MemoRize/src/main/java/game/model/GameData.java
@@ -1,4 +1,65 @@
 package game.model;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+
 public class GameData {
+    private Group mainGroup;
+    private Scene mainScene;
+    private Button[] buttons;
+    private Boolean[] haveToPress;
+    private Button start;
+
+    public GameData(){
+
+    }
+
+    public Scene menuView(){
+        Group group= new Group();
+        Scene main= new Scene(group,600,400);
+
+        return main;
+    }
+
+    public Group getMainGroup() {
+        return mainGroup;
+    }
+
+    public void setMainGroup(Group mainGroup) {
+        this.mainGroup = mainGroup;
+    }
+
+    public Scene getMainScene() {
+        return mainScene;
+    }
+
+    public void setMainScene(Scene mainScene) {
+        this.mainScene = mainScene;
+    }
+
+    public Button[] getButtons() {
+        return buttons;
+    }
+
+    public void setButtons(Button[] buttons) {
+        this.buttons = buttons;
+    }
+
+    public Boolean[] getHaveToPress() {
+        return haveToPress;
+    }
+
+    public void setHaveToPress(Boolean[] haveToPress) {
+        this.haveToPress = haveToPress;
+    }
+
+    public Button getStart() {
+        return start;
+    }
+
+    public void setStart(Button start) {
+        this.start = start;
+    }
 }
