1d6f71e53c52346d7f7ea5ce53039c5e416a21f2
robicsirak99 <robi.csirak99@gmail.com>
2019-09-24 14:22:11 +0200

    PlayerData osztály hozzáadása és az SQLPersistance osztály megírásának elkezdése

---
+++ MemoRize/src/main/java/game/model/PlayerData.java
@@ -0,0 +1,23 @@
+package game.model;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "felhasznalo_adatai")
+public class PlayerData {
+    @Id
+    private int user_id;
+    @Column(name = "nev")
+    private String nev;
+    @Column(name = "kor")
+    private int kor;
+    @Column(name = "nem")
+    private String nem;
+    @Column(name = "kitoltes_ideje")
+    private Date kitoltes_ideje;
+
+    public void setNev(String nev) { this.nev = nev; }
+    public void setKor(int kor) { this.kor = kor; }
+    public void setNem(String nem) { this.nem = nem; }
+    public void setKitoltes_ideje(Date kitoltes_ideje) { this.kitoltes_ideje = kitoltes_ideje; }
+}
--- MemoRize/src/main/java/game/model/SQLPersistance.java
+++ MemoRize/src/main/java/game/model/SQLPersistance.java
@@ -8,4 +8,25 @@ public class SQLPersistance {
 
     private static EntityManager em;
 
+    public void insert(PlayerData playerData){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-persistence-unit-1");
+        em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(playerData);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+
+    /*public GameState read_table(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-persistence-unit-1");
+        em = emf.createEntityManager();
+        GameState gameState= em.find(GameState.class, 2);
+        em.close();
+        emf.close();
+        return gameState;
+    }*/
+
 }
